cb6752291e1e12a88fa1bb052c3ee282848ffa40
francus11 <francus62@gmail.com>
2026-08-31 19:30:16 +0200

    added validation to the training process added model loading

--- crnn.py
+++ crnn.py
@@ -201,7 +201,8 @@ def train_crnn(
     batch_size = 8,
     max_epochs = 10,
     learning_rate = 1e-3,
-    resume_dir = None
+    resume_dir = None,
+    model = None
 ):
     # =============================================
     # Load and preprocess the dataset
@@ -216,11 +217,15 @@ def train_crnn(
     
     df_train, df_val, df_test = common.get_subsets(df_words, test_size=0.2, val_size=0.20)
 
-    dataset = HandwritingDataset(df_train)
+    train_dataset = HandwritingDataset(df_train)
+    val_dataset = HandwritingDataset(df_val)
+    test_dataset = HandwritingDataset(df_test)
     
     generator = torch.Generator().manual_seed(common.GLOBAL_SEED)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, generator=generator, collate_fn=collate_fn_pad)
-    
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, generator=generator, collate_fn=collate_fn_pad)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, generator=generator, collate_fn=collate_fn_pad)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, generator=generator, collate_fn=collate_fn_pad)
+
     #endregion
     
     # =============================================
@@ -229,7 +234,8 @@ def train_crnn(
     #region
     
     # Inicjalizacja modelu, funkcji straty i optymalizatora
-    model = PretrainResnetClassifier(num_classes=len(df_train["user_class"].unique()), embedding_dim=embedding_dim)
+    if model is None:
+        model = PretrainResnetClassifier(num_classes=len(df_train["user_class"].unique()), embedding_dim=embedding_dim)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     
@@ -265,7 +271,7 @@ def train_crnn(
             "embedding_dim": embedding_dim,
             "batch_size": batch_size,
             "learning_rate": learning_rate,
-            "dataset": type(dataset).__name__,
+            "dataset": type(train_dataset).__name__,
             "model": type(model).__name__,
             "encoder": type(model.encoder).__name__,
             "criterion": type(criterion).__name__,
@@ -302,13 +308,16 @@ def train_crnn(
     model.train()
     
     for epoch in range(start_epoch, max_epochs):
-        epoch_start_time = time.time()
         
         running_loss = 0.0
         correct = 0
         total = 0
 
-        for batch_idx, (images, labels) in enumerate(dataloader):
+        # ===== train =====
+        model.train()
+        train_epoch_start_time = time.time()
+        
+        for batch_idx, (images, labels) in enumerate(train_dataloader):
             optimizer.zero_grad()
             
             # Przepływ w przód
@@ -325,15 +334,44 @@ def train_crnn(
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-        epoch_loss = running_loss / total
-        epoch_acc = (correct / total) * 100
-        epoch_time = time.time() - epoch_start_time
+        train_epoch_time = time.time() - train_epoch_start_time
+        
+        train_epoch_loss = running_loss / total
+        train_epoch_acc = (correct / total) * 100
+        
+        # ===== validate =====
+        model.eval()
+        val_epoch_start_time = time.time()
+        
+        with torch.no_grad():
+            val_running_loss = 0.0
+            val_correct = 0
+            val_total = 0
+
+            for images, labels in val_dataloader:
+                logits, _ = model(images)
+                loss = criterion(logits, labels)
+
+                val_running_loss += loss.item() * images.size(0)
+                _, predicted = torch.max(logits, 1)
+                val_total += labels.size(0)
+                val_correct += (predicted == labels).sum().item()
+
+            val_epoch_loss = val_running_loss / val_total
+            val_epoch_acc = (val_correct / val_total) * 100
+        
+        val_epoch_time = time.time() - val_epoch_start_time
+        
+        # ===== metrics =====
         epoch_metrics = {
             "epoch": epoch + 1,
-            "loss": epoch_loss,
-            "accuracy": epoch_acc,
             "learning_rate": optimizer.param_groups[0]["lr"],
-            "time_seconds": epoch_time,
+            "train_loss": train_epoch_loss,
+            "train_accuracy": train_epoch_acc,
+            "train_time_seconds": train_epoch_time,
+            "val_loss": val_epoch_loss,
+            "val_accuracy": val_epoch_acc,
+            "val_time_seconds": val_epoch_time,
         }
         metrics_history.append(epoch_metrics)
 
@@ -357,7 +395,7 @@ def train_crnn(
             generator,
         )
 
-        print(f"Epoka [{epoch+1}/{max_epochs}] | Straty (Loss): {epoch_loss:.4f} | Dokładność (Acc): {epoch_acc:.2f}% | Czas: {epoch_time:.2f}s")
+        print(f"Epoka [{epoch+1}/{max_epochs}] | Straty (Loss): {train_epoch_loss:.4f} | Dokładność (Acc): {train_epoch_acc:.2f}% | Czas: {train_epoch_time:.2f}s")
         
     #endregion
         
